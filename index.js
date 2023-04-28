7041567fa2981ab97865f6b1f1e6dbe819d8fa2a
rahensonzam <rahensonzam@gmail.com>
2023-04-28 16:24:40 +0200

    add RemoveInboxSMS and tabulateList functions

--- index.js
+++ index.js
@@ -5,5 +5,29 @@ document.addEventListener("readystatechange", function() {
 });
 
 function main() {
-    
+	let smsVar0 = `text`
+    let smsVar1 = RemoveInboxSMS(smsVar0)
+    // console.log("smsVar1")
+    // console.log(smsVar1)
+
+    let smsVar2 = tabulateList(smsVar1)
+    console.log("smsVar2")
+    console.log(smsVar2)
+
+}
+
+// Takes string, returns string
+function RemoveInboxSMS(inputString) {
+    return inputString.replaceAll("Inbox SMS\n\n","")
+}
+
+// Takes string, returns string
+function tabulateList(inputString) {
+    if(inputString.indexOf("$") != -1){
+        throw new Error(`"$" found in string`)
+    }
+    inputString = inputString.replaceAll("\n\n","$")
+    inputString = inputString.replaceAll("\n","\t")
+    inputString = inputString.replaceAll("$","\n")
+    return inputString
 }
