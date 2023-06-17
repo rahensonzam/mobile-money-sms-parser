8da9cc1b1c6362140f02cb1aea0863e2e9fb110b
rahensonzam <rahensonzam@gmail.com>
2023-06-17 15:37:38 +0200

    add checkAbnormalLineCount function

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function main() {
     // console.log("smsVar1")
     // console.log(smsVar1)
 
+    let issuesArray = checkAbnormalLineCount(smsVar1)
+    console.log("issuesArray")
+    console.log(issuesArray.join("\n"))
+
     let smsVar2 = tabulateList(smsVar1)
     console.log("smsVar2")
     console.log(smsVar2)
@@ -21,6 +25,27 @@ function main() {
 // Takes string, returns string
 function RemoveInboxSMS(inputString) {
     return inputString.replaceAll("Inbox SMS\n\n","")
+}
+
+// Takes string, returns array
+function checkAbnormalLineCount(inputString) {
+    let inputStringArray = inputString.split("\n")
+    let lineNumbersArray = []
+    let issuesArray = []
+    for (let index = 0; index <= inputStringArray.length - 1; index++) {
+        const element = inputStringArray[index];
+        if (element === "") {
+            lineNumbersArray.push(index + 1)
+        }
+    }
+
+    for (let index = 0 + 1; index <= lineNumbersArray.length - 1; index++) {
+        if (!(lineNumbersArray[index] === lineNumbersArray[index - 1] + 3 + 1)) {
+            issuesArray.push(lineNumbersArray[index - 1])
+        }
+    }
+
+    return issuesArray
 }
 
 // Takes string, returns string
