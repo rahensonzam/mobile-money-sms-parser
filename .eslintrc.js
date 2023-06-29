d2b4334d33627a46aae4badc0584a8fa78f789d2
rahensonzam <rahensonzam@gmail.com>
2023-06-29 10:45:04 +0200

    add Eslint config files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es6: true
+    },
+    parserOptions: {
+        "ecmaVersion": 2020,
+        "sourceType": "script"
+    },
+    extends: [
+        'eslint:recommended'    
+    ]
+};
